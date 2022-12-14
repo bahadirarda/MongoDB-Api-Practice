d66d3db58195ff78a402c6068b62b082c0e912a9
Bahadır Arda <bahadirarda96@gmail.com>
2022-12-14 20:16:05 +0300

    MongoDB .NET Core - Practice

---
+++ MongoDBApiPractice/Models/BookStoreDatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace MongoDBApiPractice.Models
+{
+	public class BookStoreDatabaseSettings
+	{
+		public string ConnectionString { get; set; } = null!;
+		public string DatabaseName { get; set; } = null!;
+		public string BooksCollectionName { get; set; } = null!;
+	}
+}
+
